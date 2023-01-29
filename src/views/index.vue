304950ede982673afac0b20e2fedc2b975695a86
LiuJiancheng <ljc@qq.com>
2023-01-29 21:33:31 +0800

    若依修改初始化完成

---
+++ src/views/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>欢迎来到物业宿舍管理系统！！！</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  data() {
+    return {
+      
+    };
+  },
+  methods: {
+    
+  }
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
+
