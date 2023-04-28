3554f80f0c8ff63858f48a80b6ccdcc04dbad317
blueuuufish <123882636+blueuuufish@users.noreply.github.com>
2023-04-28 14:12:26 +0800

    Add files via upload

---
+++ src/api/apartment/user.js
@@ -0,0 +1,69 @@
+import request from '@/utils/request'
+
+// 查询用户信息列表
+export function listUser(query) {
+  return request({
+    url: '/apartment/user/list',
+    method: 'get',
+    params: query
+  })
+}
+
+export function listFdyUser(query) {
+  return request({
+    url: '/apartment/user/fdylist',
+    method: 'get',
+    params: query
+  })
+}
+
+export function getRoot() {
+  return request({
+    url: '/apartment/user/root',
+    method: 'get',
+  })
+}
+
+// 查询用户信息详细
+export function getUser(data) {
+  return request({
+    url: '/apartment/user/getUser',
+    method: 'post',
+    data: data
+  })
+}
+
+// 新增用户信息
+export function addUser(data) {
+  return request({
+    url: '/apartment/user',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改用户信息
+export function updateUser(data) {
+  return request({
+    url: '/apartment/user',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除用户信息
+export function delUser(data) {
+  return request({
+    url: '/apartment/user/delUser',
+    method: 'post',
+    data: data
+  })
+}
+
+//修改用户的时间权限
+// export function updateRoot(userId) {
+//   return request({
+//     url: '/apartment/user/' + userId,
+//     method: 'delete'
+//   })
+// }
